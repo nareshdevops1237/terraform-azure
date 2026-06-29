fbec44c1fe0559a676f562036cbafba1f118ad2b
naresh ch <nareshdevops1237@gmail.com>
2026-06-29 22:19:16 +0300

    added users

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,4 @@
 subscription_id = "a8602307-fe44-482d-9e38-26c273e1cb1a"
 location        = "East US"
 environment     = "dev"
+tenant_id       = "a8e714aa-cd76-414c-8956-67150987c503"
--- versions.tf
+++ versions.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -13,4 +17,7 @@ provider "azurerm" {
   features {}
 
   subscription_id = var.subscription_id
+}
+provider "azuread" {
+  tenant_id = var.tenant_id
 }
