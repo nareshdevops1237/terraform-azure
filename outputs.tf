bdd35d23ef9ffe04255cffccdc52056fdb0e1c91
naresh ch <nareshdevops1237@gmail.com>
2026-06-29 22:44:27 +0300

    added required files

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,17 @@ output "subnet_database_id" {
   description = "Created Azure database subnet resource ID"
   value       = azurerm_subnet.database.id
 }
+
+output "entra_user_object_ids" {
+  value = {
+    for key, user in data.azuread_user.users :
+    key => user.object_id
+  }
+}
+
+output "entra_user_upns" {
+  value = {
+    for key, user in data.azuread_user.users :
+    key => user.user_principal_name
+  }
+}
--- users.tf
+++ users.tf
@@ -26,11 +26,8 @@ locals {
   }
 }
 
-resource "azuread_user" "users" {
+data "azuread_user" "users" {
   for_each = local.entra_users
 
-  display_name        = each.value.display_name
   user_principal_name = each.value.user_principal_name
-  mail_nickname       = each.value.mail_nickname
-  account_enabled     = true
 }
