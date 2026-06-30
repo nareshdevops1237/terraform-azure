be23fbdec3024679fd7f4af401851cbaa91b299f
naresh ch <nareshdevops1237@gmail.com>
2026-07-01 00:42:59 +0300

    added location change to southindia

--- main.tf
+++ main.tf
@@ -37,3 +37,93 @@ resource "azurerm_subnet" "database" {
   virtual_network_name = azurerm_virtual_network.lab.name
   address_prefixes     = ["10.10.2.0/24"]
 }
+
+
+# --- Network Interfaces ---
+resource "azurerm_network_interface" "app_nic" {
+  name                = "nic-vm-app"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.application.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface" "db_nic" {
+  name                = "nic-vm-db"
+  location            = azurerm_resource_group.lab.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.database.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# --- Application VM ---
+resource "azurerm_linux_virtual_machine" "app_vm" {
+  name                = "vm-app01"
+  resource_group_name = azurerm_resource_group.lab.name
+  location            = azurerm_resource_group.lab.location
+  size                = "Standard_B2als_v2"
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.app_nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "9-lvm"
+    version   = "latest"
+  }
+
+
+}
+
+# --- Database VM ---
+resource "azurerm_linux_virtual_machine" "db_vm" {
+  name                = "vm-db01"
+  resource_group_name = azurerm_resource_group.lab.name
+  location            = azurerm_resource_group.lab.location
+  size                = "Standard_B2als_v2"
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.db_nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "9-lvm"
+    version   = "latest"
+  }
+
+
+}
