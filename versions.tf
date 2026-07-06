31b315db4e06eef99c6b587bd31efdd95db10b7c
naresh ch <nareshdevops1237@gmail.com>
2026-07-07 01:47:33 +0300

    modifiled

--- versions.tf
+++ versions.tf
@@ -20,7 +20,7 @@ provider "azurerm" {
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 
-  subscription_id = var.subscription_id
+  
 }
 provider "azuread" {
   tenant_id = var.tenant_id
